48efb29f432206eb985ebf8b04f222f4d5155aa3
Ayman <ayman.a.samy.m@gmail.com>
2018-05-11 20:13:45 +0200

    Finished General Algorithm and set the output in the right form

--- src/general_algorithm/general_algorithm.py
+++ src/general_algorithm/general_algorithm.py
@@ -1,19 +1,41 @@
 import random
-from src.roots_finder.equations_parser import *
+from src.roots_finder.open_methods.Newton_Raphson import *
+from src.general_algorithm.delete_dublication import *
+import timeit
 
-class General_Algorithm:
+class general_algorithm:
 
-
-    def solve(self, equation, num_terms=10, num_iterations=100):
+    def solve(self, equation, num_terms=10, num_iterations=100,epsilon=10**-5):
+        start_time = timeit.default_timer()
         expression = equation_to_expression(equation)
+        x = get_symbol(expression)
+        func = expression_to_lambda(x, expression)
         coefficients = to_polynomial(expression, num_terms)
-        poly_sols = solve_polynomial(coefficients, num_iterations)
+        makluin_func = lambda x: polynomial_calculater(coefficients, x)
+        poly_sols = solve_polynomial(coefficients, num_iterations=num_iterations)
+        solver = NewtonRaphson()
+        float_set = delete_dublication()
+        float_set.set_epsilon(epsilon)
         for i in range(len(poly_sols)):
-            print(poly_sols[i]);
-            #solve by newton
+            cur_solution = solver.solve(equation, poly_sols[i])[3]
+            if(abs(func(cur_solution)) < epsilon):      #if this solution is zero
+                float_set.add_element(cur_solution)
+        roots = float_set.get_elements()
+        execution_time = timeit.default_timer() - start_time
+        return num_iterations, roots, execution_time, func, makluin_func
 
 
-def solve_polynomial(coefficients, num_iterations=1000, roots_prev=[]):
+def polynomial_calculater(coefficients, x):
+    n = len(coefficients)
+    res = 0
+    tmp = 1
+    for i in range(n):
+        res += coefficients[i] * tmp
+        tmp *= x
+    return res
+
+# Remvoe Zeros in the last coefficients and set the last coefficients equal to 1
+def reduce_polynomial(coefficients):
     n = len(coefficients)
     while (True):
         if (abs(coefficients[n - 1]) < 10 ** -5):
@@ -23,20 +45,17 @@ def solve_polynomial(coefficients, num_iterations=1000, roots_prev=[]):
             break
     for i in range(n):
         coefficients[i] /= coefficients[n - 1]
+    return coefficients
 
-    def f_(x):
-        res = 0
-        tmp = 1
-        for i in range(n):
-            res += coefficients[i] * tmp
-            tmp *= x
-        return res
-
+def solve_polynomial(coefficients, num_iterations=100, roots_prev=[]):
+    coefficients = reduce_polynomial(coefficients)
+    n = len(coefficients)
+    # Initialization The First Guess
     if (len(roots_prev) == 0):
-        first_root = random.randint(2, 10)
-        roots_prev = [first_root]  # intial guess
-        for i in range(1, n - 1):
-            roots_prev.append(roots_prev[i - 1] * first_root)
+        roots_prev = []
+        for i in range(1, n):
+            roots_prev.append(i)
+    # Iterate
     roots_next = [None] * (n - 1)
     for it in range(num_iterations):
         for i in range(n - 1):
@@ -44,6 +63,7 @@ def solve_polynomial(coefficients, num_iterations=1000, roots_prev=[]):
             for j in range(n - 1):
                 if (i != j):
                     denominator *= (roots_prev[i] - roots_prev[j])
-            roots_next[i] = roots_prev[i] - f_(roots_prev[i]) / denominator
+            roots_next[i] = roots_prev[i] - polynomial_calculater(coefficients, roots_prev[i]) / denominator
         roots_prev = list(roots_next)
+
     return roots_next
